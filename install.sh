c9b996feb2206a6b39979768f5262cbcc8f1864f
miha75vu-bit <miha75vu@gmail.com>
2026-03-20 18:17:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-
 set -e
 
 OPKG_BIN="$(command -v opkg || true)"
-
 if [ -z "$OPKG_BIN" ]; then
   echo "opkg не найден. Запустите установку в среде Entware."
   exit 1
 fi
 
-$OPKG_BIN update
-$OPKG_BIN install curl
+# Ставим curl только если его нет
+if ! command -v curl >/dev/null 2>&1; then
+  "$OPKG_BIN" update
+  "$OPKG_BIN" install curl
+fi
 
 mkdir -p /opt/bin
 
-echo "Скачиваю Flashkeen..."
-curl -L -s "https://github.com/miha75vu-bit/Flashkeen/releases/download/Flashkeen1.4/flashkeen" \
+echo "Скачиваю Flashkeen (latest release)..."
+curl -fL -s "https://github.com/miha75vu-bit/Flashkeen/releases/latest/download/flashkeen" \
   -o /opt/bin/flashkeen
 
 chmod +x /opt/bin/flashkeen
